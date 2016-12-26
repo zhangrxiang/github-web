5dede35177d941c91df872a0b818c8883986d20e
zhangrxiang <zhangrxiang@yahoo.com>
2016-12-26 17:22:31 +0800

    A system of notation is a set of written symbols that are used to represent something such as music or mathematics.

--- js/data.js
+++ js/data.js
@@ -5,4 +5,8 @@
 var data = {
     "Convention": "英 [kən'venʃ(ə)n]  美 [kən'vɛnʃən] n. 大会；[法] 惯例；[计] 约定；[法] 协定；习俗",
     "assumes ": "assumes assumes assumes assumes ",
+    "maintenance ":"maintenance maintenance maintenance",
+    "encounter" : "Every day of our lives we encounter major and minor stresses of one kind or another. ",
+    "clashes " :"When people clash, they fight, argue, or disagree with each other.",
+    "notation ":"A system of notation is a set of written symbols that are used to represent something such as music or mathematics.",
 }
