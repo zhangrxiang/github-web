263cb96024cb2c0ac46763b0b79a564b032ad8e8
zhangrxiang <zhangrxiang@yahoo.com>
2016-12-26 15:04:14 +0800

    Convention

--- js/data.js
+++ js/data.js
@@ -3,19 +3,5 @@
  */
 
 var data = {
-    "Interpreter": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter1": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter2": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter3": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter4": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter5": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter6": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter7": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter8": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter9": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "Interpreter0": "Interpreter,Interpreter,Interpreter,Interpreter,",
-    "specify ": "specify specify specify specify specify specify specify ",
-    "characters ": "characters  characters characters ",
-    "comment ": "comment comment comment comment comment comment ",
-    "executable ": "executable executable executable executable 5"
+    " Convention ":"英 [kən'venʃ(ə)n]  美 [kən'vɛnʃən] n. 大会；[法] 惯例；[计] 约定；[法] 协定；习俗"
 }
