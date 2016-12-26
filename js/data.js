d3bd2d4f24cf86978f0c0bcf904175c8280baafc
zhangrxiang <zhangrxiang@yahoo.com>
2016-12-26 14:51:00 +0800

    add data.js

---
+++ js/data.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zhangrxiang on 2016/12/26.
+ */
+
+var data = {
+    "Interpreter": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter1": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter2": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter3": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter4": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter5": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter6": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter7": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter8": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter9": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "Interpreter0": "Interpreter,Interpreter,Interpreter,Interpreter,",
+    "specify ": "specify specify specify specify specify specify specify ",
+    "characters ": "characters  characters characters ",
+    "comment ": "comment comment comment comment comment comment ",
+    "executable ": "executable executable executable executable 5"
+}
