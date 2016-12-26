ab050a805b8e7b21f0e9e10deaf3bd17e8332bae
zhangrxiang <zhangrxiang@yahoo.com>
2016-12-26 17:27:07 +0800

    customary

--- js/data.js
+++ js/data.js
@@ -3,6 +3,7 @@
  */
 
 var data = {
+    "customary 英 ['kʌstəm(ə)rɪ]  美 ['kʌstə'mɛri] ":"Customary is used to describe things that people usually do in a particular society or in particular circumstances. 通常的",
     "Convention": "英 [kən'venʃ(ə)n]  美 [kən'vɛnʃən] n. 大会；[法] 惯例；[计] 约定；[法] 协定；习俗",
     "assumes ": "assumes assumes assumes assumes ",
     "maintenance ":"maintenance maintenance maintenance",
