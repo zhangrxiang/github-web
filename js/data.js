b4e1a372093195710e254de705cb75850baf240a
zhangrxiang <zhangrxiang@yahoo.com>
2017-01-09 10:10:55 +0800

    add

--- js/data.js
+++ js/data.js
@@ -5,5 +5,10 @@
 var data = {
     "concepts":"This page discusses some general concepts that will be useful as you begin to explore Drupal.",
     "Profile":"which may have fields like Name, Job Title, Department, Email, Phone Number",
-    "taxonomy":" Entity types are used to store and display data, which can be nodes (content), comments, taxonomy terms, user profiles or something custom developed."
+    "taxonomy":" Entity types are used to store and display data, which can be nodes (content), comments, taxonomy terms, user profiles or something custom developed.",
+    "represent":"A user is a type of entity which represents a real-world website user",
+    "Module":"Core modules are those included with the main download of Drupal. These can be turned on or off without downloading additional components. Examples include Blog, Book, Poll, or Taxonomy.Contributed modules are downloaded from the Modules download section of drupal.org, and installed within your Drupal installation. Examples include Panels, Views or Metatag.Custom modules are modules you write yourself. This requires a thorough understanding of Drupal, PHP programming, and Drupal's API.",
+    "Regions ":"Pages on your Drupal site are laid out in Regions. ",
+    "Blocks ":"Blocks are discrete chunks of information that are displayed in the regions of your site's pages.",
+    "Menus":"The Main menu is built by site administrators and displayed automatically in the page header of many themes (and if not, you can enable their blocks to display them).Management is the administration menu, and is presented in the Admin toolbar.Navigation is a catch-all menu that usually contains links supplied by modules on your site.User menu contains links to the User account and the logout link."
 };
