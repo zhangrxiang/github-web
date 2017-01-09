946f314a9876e69c9378765faa9fc84f4d6cd47a
zhangrxiang <zhangrxiang@yahoo.com>
2017-01-09 10:02:32 +0800

    drupal

--- js/data.js
+++ js/data.js
@@ -3,11 +3,7 @@
  */
 
 var data = {
-    "customary  ":"英 ['kʌstəm(ə)rɪ]  美 ['kʌstə'mɛri] Customary is used to describe things that people usually do in a particular society or in particular circumstances. 通常的",
-    "Convention": "英 [kən'venʃ(ə)n]  美 [kən'vɛnʃən] n. 大会；[法] 惯例；[计] 约定；[法] 协定；习俗",
-    "assumes ": "assumes assumes assumes assumes ",
-    "maintenance ":"maintenance maintenance maintenance",
-    "encounter" : "Every day of our lives we encounter major and minor stresses of one kind or another. ",
-    "clashes " :"When people clash, they fight, argue, or disagree with each other.",
-    "notation ":"A system of notation is a set of written symbols that are used to represent something such as music or mathematics.",
-}
+    "concepts":"This page discusses some general concepts that will be useful as you begin to explore Drupal.",
+    "Profile":"which may have fields like Name, Job Title, Department, Email, Phone Number",
+    "taxonomy":" Entity types are used to store and display data, which can be nodes (content), comments, taxonomy terms, user profiles or something custom developed."
+};
