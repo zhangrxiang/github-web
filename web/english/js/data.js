ffac98de767d9e855cf43d4f8cd92cad0faa49c8
zhangrxiang <599490911@qq.com>
2017-12-13 16:38:39 +0800

    l1 p2

--- web/english/js/data.js
+++ web/english/js/data.js
@@ -3,14 +3,24 @@
  */
 
 var data = {
-    "abroad n": "He lived abroad for many years, he knows several foreign languages.",
-    "affect vt": "The small amount of rain last year affected the growth of crops.",
-    "attend vt": "All children over seven must attend school.",
-    "blame n": "I have done my best, neither praise nor blame can affect me now.",
-    "bubble n": "Children like to make bubbles with soap and water.",
-    "cemetery n": "There are many tombs in the cemetery.",
-    "commendation n": "He was given a commendation for bravery after he saved the little children from the fire.",
-    "conflict n": "Some people think that there is a great deal of conflict between religion and science.",
-    "cooperate vi": "The children cooperated with their teachers in keeping their classroom clean.",
-    "curious adj": "I am curious to know what my teacher said to my mother."
+    "abroad n.": "He lived abroad for many years, he knows several foreign languages.",
+    "affect vt.": "The small amount of rain last year affected the growth of crops.",
+    "attend vt.": "All children over seven must attend school.",
+    "blame n.": "I have done my best, neither praise nor blame can affect me now.",
+    "bubble n.": "Children like to make bubbles with soap and water.",
+    "cemetery n.": "There are many tombs in the cemetery.",
+    "commendation n.": "He was given a commendation for bravery after he saved the little children from the fire.",
+    "conflict n.": "Some people think that there is a great deal of conflict between religion and science.",
+    "cooperate vi.": "The children cooperated with their teachers in keeping their classroom clean.",
+    "curious adj.": "I am curious to know what my teacher said to my mother.",
+    "delicious adj.": "We had some delicious cakes after dinner.",
+    "direct vt.": "There was nobody to direct the workman. Which is the most direct way to London?",
+    "draw vt.": "Draw your chair nearer to the table.",
+    "empire n.": "The United States was once a part of the British Empire.",
+    "event n.": "The discovery of America was a great event.",
+    "failure n.": "Success came after many failures.",
+    "file n.": "Please put these letters in the main file. The secretary filed the cards in order.",
+    "frank adj.": "He was frank to admit that he had not studied the lesson.",
+    "generate vt.": "We know that heating water can generate steam.",
+    "halt vt.": "The soldiers halted for a rest."
 };
